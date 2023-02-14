74523132c0010212cc65c05f78c2cae6a427139b
tobseyltd <mail@tobsey.limited>
2023-02-14 11:09:17 +0100

    commit #2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-//GLOBAL VARIABLES
-const total_Array = [];
-
-//FUNCTION
 document.getElementById('submit').onclick = () => {
 
     if (document.querySelector('#expense-type').value.length === 0 ||
@@ -15,38 +11,36 @@ document.getElementById('submit').onclick = () => {
     else {
 
         // Need to declare variables for formatting date and expense value to european standard //
+
         const formatted_date_DE = new Date(document.querySelector('#date').value);
         const expense_Value = document.querySelector('#expense').value;
+
         //////////////////////////////////////////////////////////////////////////////////////////
 
         document.getElementById('add-expense').innerHTML += `
-            <tr id='expense'>
+            <tr id='expense-row'>
                 <td>${document.querySelector('#expense-type').value}</th>
                 <td id='expense'>${new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(expense_Value)}</td>
                 <td>${document.querySelector('#payment-type').value}</td>
                 <td id='row-date'>${new Intl.DateTimeFormat('de-DE', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }).format(formatted_date_DE)}</td>
                 <td><img id="delete-button" src="./media/delete.png"/></td>
             </tr>`;
-
-        document.querySelectorAll('#delete-button').forEach((deleteButton) => {
-            deleteButton.onclick = () => {
-                document.getElementById('expense').remove();
-            };
-        });
-
-        total_Array.push(Number(expense_Value));
+           
     };
 
-    // Need to declare variables for calculating and formatting total sum of expenses //
-    let total_Result = total_Array.reduce((a, b) => { return a + b; });
-    let formatted_total_Result = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(total_Result)
-    ////////////////////////////////////////////////////////////////////////////////////
-    
-    document.getElementById('total').innerHTML = formatted_total_Result;
+    document.querySelectorAll('#expense-row').forEach((row) => {
+        row.querySelectorAll('#delete-button').forEach(deleteButton => {
+            deleteButton.onclick = () => {
+                row.remove();                    
+            };
+        });
+    });
 
     document.querySelector('#expense-type').value = '';
     document.querySelector('#expense').value = '';
     document.querySelector('#payment-type').value = '';
     document.querySelector('#date').value = 'tt.mm.jjjj';
 };
+
+
 
